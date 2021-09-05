08ed2bf1517e966094ec9389d7a28bff78b0b1d7
Erick Lumunge <lumungep12@gmail.com>
2021-09-06 02:42:46 +0300

    resources

--- src/Pages/Resources/Resources.jsx
+++ src/Pages/Resources/Resources.jsx
@@ -5,6 +5,9 @@ const Resources = () => {
         <div>
 			<br /><br /><br /><br /><br />
 			<Typography variant="h4">Articles</Typography>
+            <a href="https://www.cprogramming.com/whatdoesittake.html"> What it takes to be a programmer </a>
+            <a href="https://www.cprogramming.com/whyprogram.html"> Why program </a>
+            <a href="https://www.cprogramming.com/tutorial/thinking.html"> Thinking like a programmer </a>
 			<Typography variant="h4">Interesting Videos</Typography>
 			<Typography variant="h4">Books</Typography>
 			<Typography variant="h4">Papers</Typography>

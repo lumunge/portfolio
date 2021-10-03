9fef07630bc89d9e9115cbea9838bc62817510b8
Erick Lumunge <lumungep12@gmail.com>
2021-10-03 20:06:13 +0300

    build 3

--- src/Pages/Contact/styles.js
+++ src/Pages/Contact/styles.js
@@ -1,36 +1,30 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
-	// main: {
-	// 	paddingTop: "5rem",
-	// 	height: "100%",
-	// 	width: "100%",
-	// 	backgroundColor: "yellow",
-	// 	overflowX: "hidden",
-	// 	"@media (max-width: 780px)": {
-	// 		display: "flex",
-	// 		flexDirection: "column",
-	// 		alignItems: "center",
-	// 		justifyContent: "center",
-	// 		textAlign: "center",
-	// 		overflowX: "hidden",
-	// 		margin: 0,
-	// 		padding: 0,
-	// 		height: "100%",
-	// 		width: "100%",
-	// 	},
-	// },
-	// header: {
-	// 	padding: "0 0 2rem 0",
-	// },
-	// section: {
-	// 	padding: "2rem 0",
-	// },
+	main: {
+		paddingTop: "5rem",
+		height: "100%",
+		width: "100%",
+		overflowX: "hidden",
+		"@media (max-width: 780px)": {
+			display: "flex",
+			flexDirection: "column",
+			alignItems: "center",
+			justifyContent: "center",
+			textAlign: "center",
+		},
+	},
+	header: {
+		padding: "0 0 2rem 0",
+	},
+	section: {
+		padding: "2rem 0",
+	},
 	headings: {
 		color: "#f39c12",
 	},
 	card: {
-		// padding: "2rem 1rem",
+		padding: "2rem 1rem",
 		display: "flex",
 		flexDirection: "column",
 		alignItems: "center",
@@ -47,8 +41,8 @@ export default makeStyles(() => ({
 		textAlign: "center",
 		backgroundColor: "#f39c12",
 		color: "#17202a",
-		// margin: "4rem 0",
-		// padding: "4rem 0",
+		margin: "4rem 0",
+		padding: "4rem 0",
 	},
 	quote: {
 		marginTop: "1.5rem",
@@ -57,7 +51,7 @@ export default makeStyles(() => ({
 		backgroundColor: "#f39c12",
 	},
 	button: {
-		// marginTop: "1rem",
+		marginTop: "1rem",
 	},
 	link: {
 		textDecoration: "none",
@@ -68,10 +62,10 @@ export default makeStyles(() => ({
 		height: "2px",
 		backgroundColor: "#f39c12",
 		border: "none",
-		// margin: "10px 0",
+		margin: "10px 0",
 	},
 	caption: {
 		color: "#f39c12",
-		// paddingBottom: "10px",
+		paddingBlock: "10px",
 	},
 }));

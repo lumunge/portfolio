e5adc94c177acfc4d9537b77a55d0ce0215ce6c8
Erick Lumunge <lumungep12@gmail.com>
2021-10-05 04:06:35 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -17,31 +17,11 @@ const App = () => {
 			<Scroll>
 				<Navbar />
 				<Switch>
-					<Route
-						exact
-						path={process.env.PUBLIC_URL + "/"}
-						component={Home}
-					/>
-					<Route
-						exact
-						path={process.env.PUBLIC_URL + "/work"}
-						component={Work}
-					/>
-					<Route
-						exact
-						path={process.env.PUBLIC_URL + "/about"}
-						component={About}
-					/>
-					<Route
-						exact
-						path={process.env.PUBLIC_URL + "/contact"}
-						component={Contact}
-					/>
-					<Route
-						exact
-						path={process.env.PUBLIC_URL + "/resources"}
-						component={Resources}
-					/>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/work" component={Work} />
+					<Route exact path="/about" component={About} />
+					<Route exact path="/contact" component={Contact} />
+					<Route exact path="/resources" component={Resources} />
 				</Switch>
 				<Footer />
 			</Scroll>

d3e2c701c48de8ccb370ee98118490bd608b412b
lumunge <lumungep12@gmail.com>
2022-04-09 22:51:08 +0300

    app

--- src/App.js
+++ src/App.js
@@ -6,23 +6,25 @@ import Scroll from "./Components/Scroll";
 // pages
 import Home from "./Pages/Home/Home";
 import Work from "./Pages/Work/Work";
+import Articles from "./Pages/Articles/Articles";
 import Resources from "./Pages/Resources/Resources";
 import "./index.css";
 
 const App = () => {
-	return (
-		<div className="App">
-			<Scroll>
-				<Navbar />
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route exact path="/work" component={Work} />
-					<Route exact path="/resources" component={Resources} />
-				</Switch>
-				<Footer />
-			</Scroll>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Scroll>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/work" component={Work} />
+          {/* <Route exact path="/articles" component={Articles} /> */}
+          <Route exact path="/resources" component={Resources} />
+        </Switch>
+        <Footer />
+      </Scroll>
+    </div>
+  );
 };
 
 export default App;
